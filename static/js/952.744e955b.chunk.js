3bacd5e5b130fe227a5840e3b76a5dd55d04b4d2
mehrnoosh <m.norouzi.dev23@gmail.com>
2024-06-29 16:51:53 +0430

    Updates

---
+++ static/js/952.744e955b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshopping_cart_project=self.webpackChunkshopping_cart_project||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.744e955b.chunk.js.map
